72b78526a7485d64459848da0963c380edd6b6df
phannghi2002 <136423471+phannghi2002@users.noreply.github.com>
2024-11-26 14:32:46 +0700

    weather app

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.weather"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.weather"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+
+
+
+}
---
+++ app/src/main/java/com/example/weather/Activitis/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.weather.Activitis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weather.Adapters.HourlyAdapters;
+import com.example.weather.Domains.Hourly;
+import com.example.weather.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView.Adapter adapterHourly;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerview();
+setVariable();
+
+
+    }
+
+    private void setVariable() {
+        TextView next7dayBtn = findViewById(R.id.nextBtn);
+        next7dayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FutureActivity.class));
+            }
+        });
+    }
+
+    private void initRecyclerview() {
+        ArrayList<Hourly> items = new ArrayList<>();
+
+        items.add(new Hourly("9 pm", 28, "cloudy"));
+        items.add(new Hourly("11 pm", 29, "sunny"));
+        items.add(new Hourly("12 pm", 30, "wind"));
+        items.add(new Hourly("1 am", 29, "rainy"));
+        items.add(new Hourly("2 am", 27, "storm"));
+
+        recyclerView = findViewById(R.id.view1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+
+        adapterHourly = new HourlyAdapters(items);
+        recyclerView.setAdapter(adapterHourly);
+    }
+}
